ebd4320abf8abdf06ba6445aa889795e22c09fc5
ttv-shanu69 <156026347+ttv-shanu69@users.noreply.github.com>
2024-09-07 10:08:35 -0700

    Landing Page 1

---
+++ script.js
@@ -0,0 +1,12 @@
+// function navChange () {
+
+//     let navBar = document.getElementById('nav');
+//     let scrollValue = window.scrollY;
+
+//     if (scrollValue < 70) {
+//         navBar.classList.remove('nav_change')
+//     } else {
+//         navBar.classList.add('nav_change')
+//     };
+// };
+// window.addEventListener("scroll", navChange);
